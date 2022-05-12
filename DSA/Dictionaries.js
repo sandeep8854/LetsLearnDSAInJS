e9d80b38ff3a529f09df67d915feae3ea77c00ac
sandeep8854 <sandeepstructure@gmail.com>
2022-05-12 14:58:52 +0530

    Dict -- concept

--- DSA/Dictionaries.js
+++ DSA/Dictionaries.js
@@ -3,3 +3,42 @@
 // Dictionaries:--
 // stores data as key-value pairs
 // such as the way of phone book stores is data as names and phone numbers,
+
+function Dictionary() {
+  this.add = add;
+  this.data_store = new Array();
+  this.find = find;
+  // this.remove = remove;
+  this.showAll = showAll;
+}
+
+function add(key, value) {
+  this.data_store[key] = value;
+}
+
+function find(key) {
+  return this.data_store[key];
+}
+// function remove(key) {
+//   delete this.data_store[key];
+// }
+
+// function showAll() {
+//   for (let i = 0; i < this.data_store.key; i++) {
+//     console.log(data_store[i] + ' ');
+//   }
+// }
+function showAll() {
+  for (var key in Object.keys(this.data_store)) {
+    print(key + ' -> ' + this.data_store[key]);
+  }
+}
+
+let pbook = new Dictionary();
+pbook.add('Sandeep', '8112912494');
+pbook.add('Ram Charitra', '8005079895');
+pbook.add('Ram Ashish', '9920558063');
+console.log("Ram Ashish's Extension " + pbook.find('Ram Ashish'));
+// pbook.remove('Ram Ashish');
+
+pbook.showAll();
