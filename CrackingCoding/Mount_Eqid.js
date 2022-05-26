d8d75188e33a8d8178823da6416a475ededa29ff
sandeep8854 <sandeepstructure@gmail.com>
2022-05-26 18:33:09 +0530

    some tightly losse

--- CrackingCoding/Mount_Eqid.js
+++ CrackingCoding/Mount_Eqid.js
@@ -797,23 +797,600 @@ function facttt(n) {
   console.log(String(factorial));
 }
 
-let n = 45;
+let n = 10;
 facttt(n);
 
-
-*/
 //===========================================================
-function cloud(arr) {
-  let count = -1;
-  for (let i = 0; i < arr.length; ) {
-    if (i + 2 < arr.length && arr[i] == 0) {
-      i = i + 2;
-    } else {
-      i = i + 1;
+// function cloud(arr) {
+//   let count = -1;
+//   for (let i = 0; i < arr.length; ) {
+//     if (i + 2 < arr.length && arr[i] == 0) {
+//       i = i + 2;
+//     } else {
+//       i = i + 1;
+//     }
+//     count++;
+//   }
+//   console.log(count);
+// }
+// let arr = [0, 1, 0, 0, 0, 1, 0];
+// cloud(arr);
+
+
+
+// let  len=p.length;
+//         let  inum=0;
+//         let result=new Array(len);
+//         for(let i=1;i<=len;i++)
+//         {
+//             for(let j=0;j<len;j++)
+//             {
+//                 if(p[j]==i)
+//                 {
+//                 inum=j+1;
+//                 break;
+//                 }
+//             }
+//             for(let j=0;j<len;j++)
+//             {
+//                 if(p[j]==inum)
+//                 {
+//                     inum=j+1;
+//                     break;
+//                 }
+//             }
+//             result[i-1]=inum;
+//         }
+//         console.log(result);
+
+function equation(arr) {
+  let res = [];
+  let x = 1;
+  let y = 0;
+  while (x <= arr.length) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] == x) {
+        y = i + 1;
+        break;
+      }
     }
-    count++;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] == y) {
+        y = i + 1;
+        break;
+      }
+    }
+    res[x - 1] = y;
+    x++;
+  }
+  return res;
+}
+let arr = [5, 2, 1, 3, 4];
+let ress = equation(arr);
+console.log(ress);
+
+
+//=========================================================================
+function appendAndDEle(s, t, k) {
+  let del = 0;
+  let dell;
+  let add_ = 0;
+  if (k % 2 != 0) {
+    del = Math.floor(k / 2);
+    dell = del + 1;
+  }
+  console.log(dell);
+  let noOfAdd = k - dell;
+  console.log(noOfAdd);
+  let newStr = '';
+  // let act = s.length - dell;
+  // console.log(act);
+  for (let i = 0; i < s.length - dell; i++) {
+    newStr = newStr + s.charAt(i);
+  }
+  for (let i = t.length - noOfAdd; i < t.length; i++) {
+    newStr = newStr + t.charAt(i);
+  }
+  if (newStr == t) {
+    console.log('YES');
+  } else {
+    console.log('NO');
+  }
+}
+let s = 'hackerhappy';
+let t = 'hackerrank';
+let k = 9;
+appendAndDEle(s, t, k);
+
+
+
+// let str = 'AppDividend';
+// console.log('Original String: ', str);
+
+// let removeFirstChar = str.slice(1);
+// console.log('Removing the first character', removeFirstChar);
+
+// let removeLastChar = str.slice(0, str.length - 1);
+// console.log('Removing the last character: ', removeLastChar);
+
+// function togther(a, b) {
+//   let count = 0;
+
+//   for (let i = 0; i < a.length; i++) {
+//     for (let j = 0; j < b.length; j++) {
+//       if (a[i] == b[j]) {
+//         count++;
+//       }
+//     }
+//   }
+
+//   console.log(count);
+// }
+// let a = [3, 5, 5, 7, 8, 11];
+// let b = [5, 5, 7, 8, 10, 11];
+// togther(a, b);
+
+function deliver(prep, order) {
+  let calTime = [];
+  let map1 = new Map();
+
+  let j = 0;
+  for (let i = 0; i < prep.length; i++) {
+    let temp = prep[i] + order[j];
+    calTime.push(temp);
+    j++;
+  }
+  console.log(calTime);
+}
+//
+
+//
+
+let prep = [3, 6, 2, 3, 3];
+let order = [8, 5, 6, 2, 4];
+deliver(prep, order);
+
+
+
+// function xorSum(n) {
+//   let count = 0;
+//   // let simpleSum = 0;
+//   // let xorSum = 0;
+//   for (let i = 0; i <= n; i++) {
+//     let simpleSum = n + i;
+//     let xorSum = n ^ i;
+//     if (simpleSum == xorSum) {
+//       count++;
+//     }
+//   }
+//   console.log(count);
+// }
+// let n = 10;
+// xorSum(n);
+
+// let n = 100;
+// let i = 7;
+// console.log(' Task ' + (n ^ i));
+
+// function xorSum(n) {
+//   let ans = 0;
+//   while (n > 0) {
+//     if (n % 2 == 0) ans++;
+
+//     n = Math.floor(n / 2);
+//   }
+//   let res = Math.pow(2, ans);
+//   console.log(res);
+// }
+// let n = 10;
+// xorSum(n);
+
+
+
+
+function greatXor(n) {
+  let count = 0;
+  let xorRES = 0;
+  for (let i = 1; i < n; i++) {
+    xorRES = i ^ n;
+    if (xorRES > n) count++;
   }
   console.log(count);
 }
-let arr = [0, 1, 0, 0, 0, 1, 0];
-cloud(arr);
+
+let n = 2;
+greatXor(n);
+
+
+
+// function counterGame(n) {
+//   let str_1="Richard";
+//   let str_2="Louise";
+//   let subtrctForCounting=0;
+
+//   for(let i=n;i>=1;i--){
+//     if()
+
+//   }
+// }
+
+// let n = 32;
+// counterGame(n);
+
+function findPowerOfTwo(n) {
+  let k = n;
+  let differenceCount = 0;
+  if ((n & (n - 1)) != 0) {
+    // console.log('It is Not Power of 2');
+    n--;
+    differenceCount++;
+  } else {
+    // console.log('It is Power of 2');
+    let diff = n - k;
+    n = diff;
+    if (n == 1) {
+      console.log('Richard');
+    } else {
+      console.log('Louise');
+    }
+  }
+}
+
+let n = 14;
+findPowerOfTwo(n);
+
+
+
+
+function sansaXOR(arr) {
+  let xor = 0;
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    xor = arr[i];
+    newArr.push(xor);
+  }
+  //console.log(newArr);
+  // let j = 1;
+  for (let i = 0, j = 1; i < arr.length && j < arr.length; i++) {
+    let xor1 = arr[i] ^ arr[j];
+    newArr.push(xor1);
+    j++;
+  }
+  // console.log(newArr);
+  let suumm = 0;
+  for (let i = 0; i < arr.length; i++) {
+    suumm = suumm ^ arr[i];
+  }
+  newArr.push(suumm);
+  // console.log(newArr);
+  let res = 0;
+  for (let i = 0; i < newArr.length; i++) {
+    res = res ^ newArr[i];
+  }
+  console.log(res);
+}
+let arr = [3, 4, 5];
+sansaXOR(arr);
+
+
+//=====================================================================
+//=====================================================================
+//=====================================================================
+
+//console.log('Samde');
+let a = 10;
+let b = 14;
+
+if (a == 0) {
+  console.log('0');
+}
+let moveFactor = 0;
+while (a != b) {
+  // a = Math.floor(a / 2);
+  a >>= 1; // he give half of the number like as 10>>1 he give 5
+  // like as 50>>1 he will give 25.
+  b >>= 1;
+  //b = Math.floor(b / 2);
+  moveFactor++;
+}
+console.log(a << moveFactor); // he give double of the number ,like as 10<<1
+//he give 20
+// like as  4<<2 he will give 8,
+console.log(a);
+console.log(4 << 1); // 2
+console.log(1 << 2); //
+
+console.log(50 >> 1);
+
+console.log(1 << 3);
+
+//console.log(12 >> 1);
+
+//================================================================================
+//================================================================================
+//================================================================================
+
+
+function sansaXOR(arr) {
+  let newArray = [];
+  let len = arr.length;
+  let x = 0;
+  if (len % 2 == 0) {
+    return 0;
+  } else {
+    for (let i = 0; i < len; i += 2) {
+      x ^= arr[i];
+    }
+  }
+  return x;
+}
+
+let arr = [3, 4, 5];
+let re = sansaXOR(arr);
+console.log(re);
+
+//console.log(98 ^ 12); // 110
+function sansaXOR_(arr1) {
+  let len = arr1.length;
+  let x = 0;
+  if (len % 2 == 0) {
+    return 0;
+  } else {
+    for (let i = 0; i < len; i += 2) {
+      x = x ^ arr1[i];
+    }
+  }
+  return x;
+}
+
+let arr1 = [98, 74, 12];
+let reas = sansaXOR_(arr1);
+console.log('Sansa XOR:- ' + reas);
+
+//==========================================================================
+//==========================================================================
+
+
+// find the AND Product.
+
+function andProduct(a, b) {
+  let x = 0;
+  for (let i = a; i <= b; i++) {
+    x = x & b;
+  }
+  console.log(x);
+}
+let a = 10;
+let b = 14;
+andProduct(a, b);
+
+console.log(40 & 23);
+
+//============================================================================
+
+// Javascript program to find bit-wise
+// & of all numbers from x to y.
+
+// Find position of MSB in n. For example
+// if n = 17, then position of MSB is 4.
+// If n = 7, value of MSB is 3
+function msbPos(n) {
+  let msb_p = -1;
+  while (n > 0) {
+    n = n >> 1;
+    msb_p++;
+  }
+
+  return msb_p;
+}
+
+// Function to find Bit-wise & of all
+// numbers from x to y.
+function andOperator(x, y) {
+  let res = 0; // Initialize result
+
+  while (x > 0 && y > 0) {
+    // Find positions of MSB in x and y
+    let msb_p1 = msbPos(x);
+    let msb_p2 = msbPos(y);
+
+    // If positions are not same, return
+    if (msb_p1 != msb_p2) break;
+
+    // Add 2^msb_p1 to result
+    let msb_val = 1 << msb_p1;
+    res = res + msb_val;
+
+    // subtract 2^msb_p1 from x and y.
+    x = x - msb_val;
+    y = y - msb_val;
+  }
+
+  return res;
+}
+
+// Driver Code
+let x = 11,
+  y = 15;
+
+console.log(andOperator(x, y));
+
+//============================================================================
+
+// function countPosi(a) {
+//   let msb_pos = -1;
+//   while (a > 0) {
+//     a = a >> 1;
+//     msb_pos++;
+//   }
+//   console.log('count the position ' + msb_pos);
+// }
+
+// let a = 60;
+// countPosi(a);
+
+//============================================================================
+
+// function findMostSignificantBit(n) {
+//   if (n == 0) return 0;
+
+//   let msb = 0;
+//   n = Math.floor(n / 2);
+//   while (n != 0) {
+//     n = Math.floor(n / 2);
+//     msb++;
+//   }
+//   console.log(1 << msb);
+// }
+// let n = 60;
+// findMostSignificantBit(n);
+
+// //=============================================================
+// // count the position of most signigificant bit.
+
+// function countMSBPosition(num) {
+//   let count = -1;
+//   num = Math.floor(num / 2);
+//   while (n != 0) {
+//     n = Math.floor(n / 2);
+//     count++;
+//   }
+//   console.log(count);
+// }
+// let num = 60;
+// countMSBPosition(num);  // 5
+
+//===================================================
+
+function candy(arr) {
+  let one = arr[0];
+  let newArray = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > one) {
+      newArray.push(i + 1);
+    } else {
+      one++;
+    }
+  }
+}
+
+let arr = [4, 6, 4, 5, 6, 2];
+candy(arr);
+
+
+
+//==================================================
+
+function sherlockANDCoast(B) {
+  let maxVar = Math.abs(B[1] - 1);
+  let maxOne = Math.abs(B[0] - 1);
+
+  console.log(maxVar + ' == ' + maxOne);
+}
+
+let arr = [10, 1, 10, 1, 10];
+sherlockANDCoast(arr);
+
+let gfg = new Array(26);
+
+for (let i = 0; i < gfg.length; i++) {
+  gfg[i] = new Array(26);
+}
+//initializing array
+let h = 0;
+for (let i = 0; i < gfg.length; i++) {
+  for (let j = 0; j < gfg.length; j++) {
+    gfg[i][j] = h++;
+  }
+}
+
+// for printing the element
+
+for (let i = 0; i < gfg.length; i++) {
+  for (let j = 0; j < gfg.length; j++) {
+    console.log(gfg[i][j] + '  ');
+  }
+}
+
+
+function greadFunction(grid) {
+  let newArr = [];
+  //console.log(grid.length); // 3
+  for (let i = 0; i < grid.length; i++) {
+    let text = grid[i].split('').sort();
+    newArr.push(text);
+  }
+
+  let col = newArr[0].length;
+  // console.log(col);  // 3
+  for (let i = 0; i < newArr.length - 1; i++) {
+    for (let j = 0; j < col; j++) {
+      if (newArr[i][j] > newArr[i + 1][j]) {
+        console.log('NO');
+      }
+    }
+  }
+  console.log('YES');
+}
+//let newArr = [];
+let grid = ['abc', 'ade', 'efg'];
+greadFunction(grid);
+
+// let g = ['adb'];
+// let arr = [];
+// let text = g[0].split('').sort();
+// arr.push(text);
+// console.log(arr);
+
+//
+//========================================================================
+
+function canies(ratings) {
+  let sum = 0;
+  let left2right = new Array(ratings.length);
+  let right2left = new Array(ratings.length);
+  // Arrays.fill(left2right, 1);
+  // Arrays.fill(right2left, 1);
+  for (let i = 0; i < left2right.length; i++) {
+    left2right[i] = 1;
+  }
+  for (let i = 0; i < right2left.length; i++) {
+    right2left[i] = 1;
+  }
+  for (let i = 1; i < ratings.length; i++) {
+    if (ratings[i] > ratings[i - 1]) {
+      left2right[i] = left2right[i - 1] + 1;
+    }
+  }
+  for (let i = ratings.length - 2; i >= 0; i--) {
+    if (ratings[i] > ratings[i + 1]) {
+      right2left[i] = right2left[i + 1] + 1;
+    }
+  }
+  for (let i = 0; i < ratings.length; i++) {
+    sum += Math.max(left2right[i], right2left[i]);
+  }
+  return sum;
+}
+
+let arr = [2, 4, 3, 5, 2, 6, 4, 5];
+console.log(canies(arr));
+
+//===============================================================================
+
+
+function jim_AND_Order(orders, prep) {
+  let deliverTime = [];
+  let map1 = new Map();
+  for (let i = 0; i < order.length; i++) {
+    let temp = order[i] + prep[i];
+  }
+}
+let order = [8, 4, 5, 3, 4];
+let prep = [1, 2, 6, 1, 3];
+console.log(jim_AND_Order(order, prep));
+
+//============================================================
+
+*/
