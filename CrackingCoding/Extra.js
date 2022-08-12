646ad075d20ac48d2769394c959f56e8726210c4
sandeep8854 <sandeepstructure@gmail.com>
2022-08-12 14:47:27 +0530

    adding

--- CrackingCoding/Extra.js
+++ CrackingCoding/Extra.js
@@ -44,7 +44,7 @@ let res = adding(nums);
 console.log(res);
 
 //=================================================================
-*/
+
 //console.log('San');
 
 // let po = Math.pow(2); // base , exponential
@@ -77,8 +77,72 @@ console.log(res);
 // delivery.sort((a, b) => a[0] - b[0]);
 // return delivery.map(value => value[1]);
 
-let s = '';
-for (let i = 1; i <= 11; i++) {
-  s = s + i + ' ';
+// let s1 = 'xaxb';
+// let split = s1.split('');
+
+// console.log(split);
+
+// let sor = split.sort();
+// console.log(sor);
+// let resss = sor.join('');
+
+// let s2 = 'sandeep';
+// console.log(s2.split('').sort().join(''));
+
+//=============================================================================
+
+// let n = 2025;
+// console.log(typeof n);
+
+// let nu = String(n);
+// console.log(typeof nu);
+// console.log(nu.length);
+
+// console.log(Math.pow(297, 2));
+//========================================
+
+
+
+
+function unknown(s, k) {
+  let b = 66;
+  console.log(String.fromCharCode(b));
 }
-console.log(s);
+//   let newArr = [];
+//   let code = 0;
+//   for (let i = 0; i < s.length; i++) {
+//     code = s.charCodeAt(i) + k;
+//     newArr.push(code);
+//   }
+//   console.log(String.fromCharCode(code));
+// }
+
+let s = 'midd';
+let k = 2;
+unknown(s, k);
+//===========================================================================
+
+// let t = '1';
+// console.log(t.charCodeAt(0));
+
+function sherlockAndBeast(n) {
+  let result = -1;
+  let i = n;
+  while (i >= 0) {
+    if (i % 3 == 0) {
+      result = '5'.repeat(i) + '3'.repeat(n - i);
+      break;
+    }
+    i = i - 5;
+  }
+  console.log(result);
+}
+let n = 11;
+sherlockAndBeast(n);
+*/
+if (-1 >= -0) {
+  console.log('YES');
+} else {
+  console.log('No');
+}
+console.log(3 - Math.abs(-2));
